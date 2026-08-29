4b51fcf50fdee0577beb1571027283a1d58fd0ec
OswaldKasulke <rb@datenschwester.de>
2026-08-29 22:11:19 +0200

    Prepare static deployment for leverkusen-makler.de

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,1 @@
-import type {MetadataRoute} from "next"; export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/"},sitemap:"https://immobilienmakler-leverkusen.de/sitemap.xml"}}
+import type {MetadataRoute} from "next"; export const dynamic = "force-static"; export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/"},sitemap:"https://leverkusen-makler.de/sitemap.xml"}}
